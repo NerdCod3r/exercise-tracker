7dc6cd34398bd426cc9af2f9042cbfd1e1993879
Apinda Tekula <nerdcod3rx@gmail.com>
2025-01-16 14:35:47 +0000

    I have completed the exercise tracker. All the tests have passed.

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ mongoose.connect(uri);
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   }
 });
 
@@ -41,7 +42,7 @@ const exerciseSchema = new mongoose.Schema({
     required: true
   },
   date: {
-    type: Date,
+    type: String,
     required: true
   }
 });
@@ -51,44 +52,45 @@ const EXERCISES = new mongoose.model('EXERCISES', exerciseSchema);
 
 app.post("/api/users", async function(request, response){
   const userName = request.body.username;
-  var add_user = false;
- await USER.find({username:`${userName}`}).then((doc)=>{
+  if ( !userName ) {
+    response.json({error: "Enter a valid username"});
+  }
+  available = false;
+  USER.find({username: `${userName}`}).then((doc)=>{
+    console.log(doc);
     if ( doc.length === 0 ) {
-      add_user = true;
+      available = true;
     }
   }).catch(err=>err);
 
-  if ( add_user ) {
-    // Create a user and add it.
-    const new_user = new USER({
-      username: `${userName}`
-    });
-
-    new_user.save().then(success =>{
-    }).catch(err=>err);
-
-    await USER.find({username:`${userName}`}).then(success=>{
-        response.json({username: success[0].username, id: success[0].id});
-    }).catch(err=>err);
-  } else {
-   await USER.find({username: `${userName}`}).then(success=>{
-      response.json({username: success[0].username, id: success[0].id});
-    }).catch(err=>err);
+  if ( available ) {
+    response.json({error: "Username already exists. Pick a new one"});
   }
+
+  const new_user = new USER({
+    username: `${userName}`
+  });
+  new_user.save().then((doc)=>{
+    response.json({username:`${userName}`, _id: doc.id});
+  }).catch(err=>err);
 });
 
 app.post("/api/users/:__id/exercises", async function(request, response){
    const user_id = request.params.__id;
    var user_found = false;
    var _date = request.body.date;
+   var userName = "";
 
   if ( !_date ) {
-    _date = new Date();
+    _date = new Date().toDateString();
+  } else {
+    _date = new Date(_date).toDateString();
   }
 
  await USER.find({_id: user_id}).then((doc)=>{
     if ( doc.length !== 0 ) {
       user_found = true;
+      userName = doc[0].username;
     }
   }).catch(err=>err);
 
@@ -106,6 +108,7 @@ if ( user_found ) {
   }).catch(err=>err);
 
   response.json({
+    username: userName,
     _id: user_id,
     description: `${request.body.description}`,
     duration: parseInt(request.body.duration),
@@ -128,40 +131,62 @@ app.get("/api/users", async function(request, response){
   response.send(Users);
 });
 
-app.get("/api/users/:_id/logs", async function(request, response){
-  // Query parameters to check for
-  const from = request.query.from;
-  const to = request.query.to;
-  const limit = request.query.limit;
-
-  console.log(`${from} to ${to}. LIMIT = ${limit}`);
+app.get('/api/users/:_id/logs', async (req, res) => {
+  const user_id = req.params._id;
+  var {from, to, limit} = req.query;
   const date_reg = /[0-9]{4}\-{1}[0-9]{1,2}\-{1}[0-9]/;
-  var valid_dates = false;
-  if (date_reg.test(from) && date_reg.test(to)) {
-    valid_dates = true;
-  } else{
-  }
 
-  const user_id = request.params._id;
-  var exerciseArray = [];
-  console.log(`user_id: ${user_id}.`);
-  EXERCISES.find({__id: `${user_id}`, date:{
-    $gt: new Date(from),
-    $lte: new Date(to)
-  }}).limit(parseInt(limit)).then((doc)=>{
-    console.log(`${user_id}'s logs:`);
-    console.log(doc);
-    for (let i = 0; i < doc.length; i++ ) {
-      exerciseArray.push({description: `${doc[i].description}`,
-      duration: parseInt(doc[i].duration),
-    date: new Date(doc[i].date).toDateString()});
+  var userName = "Unknown";
+
+  await USER.find({_id: user_id}).then(doc=>{
+   userName = doc[0].username;
+  }).catch(err=>err);
+
+ var exercise_logs = [];
+
+ await EXERCISES.find({__id: `${user_id}`}).then((doc)=>{
+  exercise_logs = doc;
+ }).catch(err=>err);
+
+ // Handle from
+ if (from && !date_reg.test(from) || to && !date_reg.test(to) ) {
+  response.json({error: 'invalid date format. \'YYYY-MM-DD\' REQUIRED'});
+ }
+ var filtered_logs_from = [];
+ if ( from && date_reg.test(from)){
+  for ( let i = 0; i < exercise_logs.length; i++ ) {
+    const curr_obj = exercise_logs[i];
+    const _date = curr_obj.date;
+
+    if (new Date(from) <= new Date(_date)) {
+
+      if (to && date_reg.test(to)) {
+        if (new Date(to) >= new Date(_date)) {
+          filtered_logs_from.push(curr_obj);
+        }
+      }else {
+        filtered_logs_from.push(curr_obj);
+      }
     }
-    response.json({'count':doc.length, log: exerciseArray});
-  }).catch(err=>{
-    console.error(err);
-});
+  }
+  exercise_logs = filtered_logs_from;
+ }
+ if (limit && !parseInt(limit)) {
+  res.json({error: 'limit should be a positive integer'});
+ }
+
+ if (limit && parseInt(limit) && exercise_logs.length > parseInt(limit)) {
+  exercise_logs = exercise_logs.slice(0, parseInt(limit));
+}
+ 
+ res.json({
+  username: `${userName}`, 
+  _id: user_id, 
+  count: exercise_logs.length,
+  log: exercise_logs
 });
 
+});
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
